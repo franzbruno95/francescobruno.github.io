99f1af6577b4637d9feac9b531230f322f67cf5b
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-08 12:58:57 +0100

    08/03

--- email-templates/subscribe-newsletter.php
+++ email-templates/subscribe-newsletter.php
@@ -4,7 +4,7 @@
 	$from =$_POST["email"];
 	
 	// Email Receiver Address
-	$receiver="info@yourdomain.com";
+	$receiver="francesco.bruno001@gmail.com";
 	$subject="Subscribe Newsletter form details";
 
 	$message = "
