be563e227c78923bf07e3989f5d648e97137f6cc
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-11 12:55:04 +0100

    ads

--- Contact.php
+++ Contact.php
@@ -1,5 +1,4 @@
 <?php 
-if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $emailFrom = $_POST['email'];
@@ -9,6 +8,6 @@ if (isset($_POST['submit'])) {
     $headers =  "From: ".$emailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
-}
+    mail($mailTo, $subject, $txt, $headers) or die("Error!");
+    echo "Thank You!";
+?>
