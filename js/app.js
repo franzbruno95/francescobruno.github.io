ceb9fad3a8cd5a141f559700e24e5f6ce814a8d4
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-11 12:17:15 +0100

    ip

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
       submitHandler: function(form) {
         $.ajax({
           type: 'POST',
-          url: 'Contact.php',
+          url: 'Contact/Contact.php',
           data: $(form).serialize(),
           success: function(data) {
             if(data.match(/success/)) {
