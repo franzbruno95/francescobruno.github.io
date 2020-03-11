8761667360465d27bbd7e82d76b913f5391f6152
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-11 11:58:37 +0100

    Update Contact.php

--- Contact/Contact.php
+++ Contact/Contact.php
@@ -1,11 +1,11 @@
 <?php 
-if (isset($_post['submit'])) {
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "francesco.bruno01@universitadipavia.com";
+    $mailTo = "francesco.bruno@francesco-bruno.com";
     $headers =  "From: ".$emailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
