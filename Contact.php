5e5986568afdc98383fe48a7b2eece4ea523cc8c
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-11 13:12:38 +0100

    dsa

--- Contact.php
+++ Contact.php
@@ -8,6 +8,7 @@
     $headers =  "From: ".$emailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers) or die("Error!");
-    echo "Thank You!";
+    mail($mailTo, $subject, $txt, $headers);
+    header ("Location: index.html");
+    
 ?>
