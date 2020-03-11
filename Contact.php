523463ef0332ab00bdc53937087bbc5c18291a3e
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2020-03-11 14:39:52 +0100

    test

--- Contact.php
+++ Contact.php
@@ -1,4 +1,5 @@
 <?php 
+    if(isset(%_POST['submit']))
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $emailFrom = $_POST['email'];
@@ -8,7 +9,12 @@
     $headers =  "From: ".$emailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
+    if(mail($mailTo, $subject, $txt, $headers)){
+        echo "<h1>sent Successfully!"." ".$name.", I will contact you shortly!<h1>";
+    }
+    else{
+        echo "Somenthing went reong!";
+    }
     header ("Location: index.html");
     
 ?>
